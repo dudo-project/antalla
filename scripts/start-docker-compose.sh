59cb412aa64756459793a637d35f7836a01b9cc8
Daniel Perez <daniel@perez.sh>
2019-10-02 23:46:42 +0100

    Add finely tuned deployment script

--- scripts/start-docker-compose.sh
+++ scripts/start-docker-compose.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+mkdir -p tmp
+COLOR_FILE="tmp/current-color.txt"
+
+current_color=$(cat $COLOR_FILE || "blue")
+if [ $current_color = "blue" ]; then
+  color="green"
+else
+  color="blue"
+fi
+
 COMPOSE_HTTP_TIMEOUT=600 \
-  exec docker-compose up -d \
+  docker-compose -p "antalla-$color" up -d \
   --no-recreate \
   --scale coinbase=2 \
   --scale binance=2 \
   --scale idex=2
+
+echo "$color" > $COLOR_FILE
+
+COMPOSE_HTTP_TIMEOUT=600 docker-compose -p "antalla-$current_color" down
